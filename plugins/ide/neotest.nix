9509b26f2e45156313b770320105982730b11901
RAPSNX <github@rapsn.me>
2026-01-14 12:59:47 +0100

    Introduce neotest

--- plugins/ide/neotest.nix
+++ plugins/ide/neotest.nix
@@ -1,21 +1,72 @@
+{ pkgs, ... }:
+let
+  neovim-ginkgo =
+    (pkgs.vimUtils.buildVimPlugin {
+      pname = "neovim-ginkgo";
+      version = "main";
+      src = pkgs.fetchFromGitHub {
+        owner = "nvim-contrib";
+        repo = "nvim-ginkgo";
+        rev = "main";
+        hash = "sha256-M4i8wyQkTV/y4QpU0743Vzdt8UEY/9LSOedZ0R2rDA8=";
+      };
+    }).overrideAttrs
+      (_old: {
+        doCheck = false;
+      });
+in
 {
+  extraPlugins = [
+    neovim-ginkgo
+    pkgs.vimPlugins.neotest-go
+  ];
+
+  extraPackages = [
+    pkgs.ginkgo
+  ];
+
+  # Only enable the plugin and configure via lua
   plugins.neotest = {
     enable = true;
-    adapters.go.enable = true;
-    lazyLoad.settings = {
-      cmd = "Neotest";
-      keys = [
-        {
-          __unkeyed-1 = "<leader>t";
-          __unkeyed-3 = "<CMD>Neotest summary<CR>";
-          desc = "🧪 Summary toggle";
-        }
-      ];
-    };
   };
+
+  keymaps = [
+    {
+      action = ''<cmd>lua require("neotest").setup({adapters = {require("neotest-go")}})<CR>'';
+      key = "<leader>to";
+      options = {
+        desc = "enable neotest-go adapter";
+      };
+      mode = [ "n" ];
+    }
+    {
+      action = ''<cmd>lua require("neotest").setup({adapters = {require("nvim-ginkgo")}})<CR>'';
+      key = "<leader>tg";
+      options = {
+        desc = "enable ginkgo adapter";
+      };
+      mode = [ "n" ];
+    }
+    {
+      action = ''<cmd>Neotest summary<CR>'';
+      key = "<leader>ts";
+      options = {
+        desc = "enable ginkgo adapter";
+      };
+      mode = [ "n" ];
+    }
+  ];
+
   extraConfigLua =
     # lua
     ''
+       require("neotest").setup({
+         adapters = {
+           require("nvim-ginkgo"),
+           require("neotest-go")
+         },
+       })
+
       -- Make q to quit floating windows
       vim.keymap.set('n', 'q', function()
         local winid = vim.api.nvim_get_current_win()
