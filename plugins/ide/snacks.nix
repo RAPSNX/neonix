2db6e304c2aa1c975053478bb38af7d2de1b6f79
RAPSNX <github@rapsn.me>
2026-08-16 19:46:50 +0200

    Open lazygit-edited files in place even when oil is the origin buffer
    The custom NeonixLazygitEdit handler always split the window before
    editing when the origin buffer's filetype was "oil", so lazygit's `e`
    opened a stacked split instead of taking over the window like it does
    for every other buffer type.

--- plugins/ide/snacks.nix
+++ plugins/ide/snacks.nix
@@ -4,7 +4,13 @@
     settings = {
       bigfile.enabled = true;
       gitbrowse.enabled = true;
-      lazygit.enabled = true;
+      lazygit = {
+        enable = true;
+        config.os = {
+          edit = ''[ -z "$NVIM" ] && (nvim -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote-send "<C-\><C-N>:NeonixLazygitEdit {{filename}}<CR>")'';
+          editAtLine = ''[ -z "$NVIM" ] && (nvim +{{line}} -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote-send "<C-\><C-N>:{{line}}NeonixLazygitEdit {{filename}}<CR>")'';
+        };
+      };
       notifier.enabled = true;
       notify.enabled = true;
       quickfile.enabled = true;
@@ -13,14 +19,34 @@
     };
   };
 
+  extraConfigLua = ''
+    vim.api.nvim_create_user_command("NeonixLazygitEdit", function(opts)
+      local origin = vim.g.neonix_lazygit_origin
+      if not origin or not vim.api.nvim_win_is_valid(origin) then
+        origin = vim.api.nvim_get_current_win()
+      end
+
+      vim.api.nvim_set_current_win(origin)
+      vim.cmd({ cmd = "edit", args = { opts.args } })
+      if opts.range > 0 then
+        vim.api.nvim_win_set_cursor(0, { opts.line1, 0 })
+      end
+    end, { nargs = 1, range = true })
+  '';
+
   keymaps = [
     {
-      action = "<cmd>lua Snacks.lazygit.open()<cr>";
+      action.__raw = ''
+        function()
+          vim.g.neonix_lazygit_origin = vim.api.nvim_get_current_win()
+          Snacks.lazygit.open()
+        end
+      '';
       key = "<leader>gg";
       options = {
         desc = "Open LazyGit";
       };
-      mode = ["n"];
+      mode = [ "n" ];
     }
     {
       action = "<cmd>lua Snacks.terminal.toggle()<cr>";
@@ -28,7 +54,7 @@
       options = {
         desc = "Toggle Terminal";
       };
-      mode = ["n"];
+      mode = [ "n" ];
     }
   ];
 
