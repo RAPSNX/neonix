33c7fe1d416572920217b384f5896a75216e09dc
RAPSNX <github@rapsn.me>
2026-09-01 00:20:45 +0200

    refactor(style): clean which-key group spec and guard navic winbar

--- plugins/ide/navic.nix
+++ plugins/ide/navic.nix
@@ -8,7 +8,14 @@
     };
 
     luaConfig.post = ''
-      vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
+      _G.neonix_navic_winbar = function()
+        local navic = package.loaded["nvim-navic"]
+        if navic and navic.is_available() then
+          return navic.get_location()
+        end
+        return ""
+      end
+      vim.o.winbar = "%{%v:lua._G.neonix_navic_winbar()%}"
     '';
   };
 }
